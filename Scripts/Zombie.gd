e52767a69fdfc790f12ab4639e23ed2f3266e397
WisartArfun <39859700+WisartArfun@users.noreply.github.com>
2019-02-16 01:56:33 +0100

    zombie added

---
+++ Scripts/Zombie.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 200
+
+var player = null
+
+func _ready():
+	add_to_group("zombies")
+
+func _physics_process(delta):
+	if player == null:
+		return
+	
+	var vec_to_player = player.global_position - global_position
+	vec_to_player = vec_to_player.normalized()
+	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+
+func kill():
+	queue_free()
+
+func set_player(p):
+	player = p
