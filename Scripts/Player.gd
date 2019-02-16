7adf306c42a2d8df49702e730f68abbdd167a3f3
Benjamin Schmid <git@schmidb.ch>
2019-02-16 02:57:04 +0100

    player change keybindings

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,12 +5,12 @@ const ROT_SPEED = 2
 
 func _physics_process(delta):
 	var move_vec = Vector2()
-	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("p1_fw") and Input.is_action_pressed("p2_fw"):
 		move_vec = Vector2(cos(global_rotation), sin(global_rotation)) * MOVE_SPEED
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("p1_fw"):
 		global_rotation += ROT_SPEED * delta
 		move_vec = Vector2(cos(global_rotation), sin(global_rotation)) * 200 * ROT_SPEED
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("p2_fw"):
 		global_rotation -= ROT_SPEED * delta
 	var collision = move_and_collide(move_vec * delta)
 	if collision:
